e58934879660e4fc9af2760f8f51ebab21127df8
aliendevuz <aliendevuz@gmail.com>
2024-03-13 12:41:49 +0500

    Upload data for testing

--- departition.py
+++ departition.py
@@ -5,7 +5,7 @@ with open(f'cache.mp4', 'wb') as out:
         data = file.read().split('\n')
 
     b = b''
-    for i in range(int(data[1])):
+    for i in range(4): #'''int(data[1])'''
         with open(f'{data[0]}/{1000 + i}', 'rb') as r:
             b += r.read()
 
--- partition.py
+++ partition.py
@@ -15,13 +15,13 @@ with open(file_name, 'rb') as f:
     byte_stream = f.read()
 
 size = len(byte_stream) - 40000
-count = size / 512000
+count = size / 2048000
 count = count if count % 1 == 0 else int(count) + 1
 
 print(f'size = {size} count = {count}')
 
 for i in range(count):
-    save_parts(i, byte_stream[i * 512000:(i + 1) * 512000])
+    save_parts(i, byte_stream[i * 2048000:(i + 1) * 2048000])
 
 with open(f"{path}/data", 'w', encoding='utf-8') as file:
     file.write(f'{path}\n{count}')
