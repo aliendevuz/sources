b146dbbcb92f780f4cda00e5b7cbbde13080bf09
aliendevuz <aliendevuz@gmail.com>
2024-03-13 10:14:53 +0500

    Upload data for testing

---
+++ departition.py
@@ -0,0 +1,12 @@
+
+
+with open(f'cache.mp4', 'wb') as out:
+    with open('video/vid/data', 'r', encoding='utf-8') as file:
+        data = file.read().split('\n')
+
+    b = b''
+    for i in range(int(data[1])):
+        with open(f'{data[0]}/{1000 + i}', 'rb') as r:
+            b += r.read()
+
+    out.write(b)
